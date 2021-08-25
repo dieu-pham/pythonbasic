b3b5876b08c6f480f7bd7f98dac4b10e345406a5
unknown <phamthedieu@gmail.com>
2021-08-25 19:16:35 +0700

    thay doi de thanh cong

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Thành thạo Python kiếm việc mới")
+print("có trong đầu sẽ có trong tay")
+print("đi rồi sẽ đến")
+print("thành công không bao giờ đến với kẻ lười biếng")
+print("cố lên Diệu ơi")
+
