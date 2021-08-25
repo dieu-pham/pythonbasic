f20e4959e9db23eaac1b700792ca9e8293220421
unknown <phamthedieu@gmail.com>
2021-08-25 20:51:11 +0700

    bài tập tam giác

---
+++ tamgiac.py
@@ -0,0 +1,11 @@
+for i in range(1,6):
+    print("*"*i)
+
+for j in range(0,5):
+    print("*"*(5-j))
+
+for k in range(1,6):
+    print(" "*(5-k)+("*"*k))
+
+for l in range(1,6):
+    print((" "*(l-1))+("*"*(6-l)))
